173cea31d6fbd0c3b07ee6bf2e12b91e47a25e53
blasty8084 <230334219+blasty8084@users.noreply.github.com>
2026-06-15 14:39:55 +0000

    Sw js

--- artifacts/sahu-csc/dev-dist/sw.js
+++ artifacts/sahu-csc/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-cce6ffed'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.o2mehnbt3oo"
+    "revision": "0.b72tbsa029o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
