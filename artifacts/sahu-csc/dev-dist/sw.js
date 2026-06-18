d5ca7a8bd7af53d9685cf64e9ee8063a113741c1
W4gki <61023093-w4gki@users.noreply.replit.com>
2026-06-18 15:31:05 +0000

    Receipt  and sw

--- artifacts/sahu-csc/dev-dist/sw.js
+++ artifacts/sahu-csc/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-cce6ffed'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.g63afruine"
+    "revision": "0.a3aq78ch8t"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
