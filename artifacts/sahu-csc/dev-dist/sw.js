6cadbe82090528e945ae065fd7f0b8f6de8c3669
blasty8084 <230334219+blasty8084@users.noreply.github.com>
2026-06-20 10:50:09 +0000

    Update Canvas design

--- artifacts/sahu-csc/dev-dist/sw.js
+++ artifacts/sahu-csc/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-cce6ffed'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.n27k9n2eh3g"
+    "revision": "0.oldrlj40h7o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
