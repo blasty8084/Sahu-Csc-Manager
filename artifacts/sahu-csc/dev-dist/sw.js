f5305a6e35f660a40e110cc494aabfc4d527a86e
X0r9y <61086911-x0r9y@users.noreply.replit.com>
2026-06-20 04:41:37 +0000

    Mockup

--- artifacts/sahu-csc/dev-dist/sw.js
+++ artifacts/sahu-csc/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-cce6ffed'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.o8vanqkq4cg"
+    "revision": "0.ufha71drfno"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
