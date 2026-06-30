ca66d79e15c1f36c20a43cf20f4571fab95b2097
Klife <61538008-klifehc@users.noreply.replit.com>
2026-06-30 08:01:31 +0000

    Replit

--- artifacts/sahu-csc/dev-dist/sw.js
+++ artifacts/sahu-csc/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-cce6ffed'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.j2u3c28h3a8"
+    "revision": "0.ap0kishe4b8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
